ec4a6964203c01b0afa2d12f67efe6470221c169
mk1323-del <mk1323@srmist.edu.in>
2026-02-24 10:29:43 +0530

    Add files via upload

---
+++ UC04.java
@@ -0,0 +1,26 @@
+/**
+ * OOPSBannerApp UC4 – Render OOPS as Banner using String Array and Loop
+ *
+ * This use case improves upon UC3 by using a String array to store banner lines
+ * and iterating through them with a for-each loop, eliminating hardcoded print
+ * statements and improving modularity and reusability.
+ *
+ * @author Developer
+ * @version 4
+ */
+
+// Extend the user story 3 to further develop the OOPS Banner Application
+// by using String arrays to hold banner lines and printing them in a loop.
+// This use case improves upon UC3 by using a string array to store banner lines
+// and iterating through them with a for-each loop, eliminating hardcoded print
+// statements and improving modularity and reusability.
+
+// Hint: …
+
+public class OOPSBannerApp {
+
+    // Main method to run the banner display
+    public static void main(String[] args) {
+
+    }
+}
